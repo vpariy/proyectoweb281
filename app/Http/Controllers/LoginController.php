edbbd609876539a87dd0d348215d47c3cd13f0b3
victor <vpariy@fcpn.edu.bo>
2023-09-22 22:34:58 -0400

    pruebas desechas

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,12 +10,6 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    // este cambio lo hice
-
-    //aun mas cambios +++
-
-    //otro cambio
-    
     public function ingreso() {
         
         return view('login.ingreso');
